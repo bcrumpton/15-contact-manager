f472127ba83d4901873ad147d9d06a09d226db96
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-02-27 18:06:32 -0600

    Reducer and reducer test js tested and develop

--- tests/reducer-test.js
+++ tests/reducer-test.js
@@ -5,9 +5,43 @@ module('reducer', () => {
     assert.ok(reducer, 'reducer exists');
   });
 
-  test('create a single contact', (assert) => {
+  test('create a single contact with an empty array', (assert) => {
     const oldState = { contacts: [] };
-    const action = { type: 'CONTACT@CREATE', data: [{ first: 'Brannon', last: 'Crumpton' }] };
-    assert.equal(reducer(oldState, action), { first: 'Brannon', last: 'Crumpton' }, 'a contact was created');
+    const action = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'John', lastName: 'Cena' }] };
+    const expectedState = { contacts: [{ firstName: 'John', lastName: 'Cena' }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
+  });
+
+  test('replace angelina with john cena', (assert) => {
+    const oldState = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+    const action = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'John', lastName: 'Cena' }] };
+    const expectedState = { contacts: [{ firstName: 'John', lastName: 'Cena' }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
+  });
+
+  test('find nic cage', (assert) => {
+    const oldState = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+    const action = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'Nic', lastName: 'Cage' }] };
+    const expectedState = { contacts: [{ firstName: 'Nic', lastName: 'Cage' }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
+  });
+
+  test('create and add johnny depp to empty array', (assert) => {
+    const oldState = { contacts: [] };
+    const action = { type: 'CONTACT@CREATE', data: { firstName: 'Johnny', lastName: 'Depp' } };
+    const expectedState = { contacts: [{ firstName: 'Johnny', lastName: 'Depp' }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
+  });
+
+  test('create and add johnny depp to array with angelina jolie', (assert) => {
+    const oldState = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+    const action = { type: 'CONTACT@CREATE', data: { firstName: 'Johnny', lastName: 'Depp' } };
+    const expectedState = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }, { firstName: 'Johnny', lastName: 'Depp' }] };
+
+    assert.deepEqual(reducer(oldState, action), expectedState);
   });
 });
